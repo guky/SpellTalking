a9abfb4dbb120ccf9778b9bb0162ebea44ff7deb
d.hlebnikovs@gmail.com <d.hlebnikovs@gmail.com>
2012-11-13 14:15:10 +0200

    Disconnect handler

---
+++ src/spelltalking/ConnectionHandlerServelet.java
@@ -0,0 +1,20 @@
+package spelltalking;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@SuppressWarnings("serial")
+public class ConnectionHandlerServelet extends HttpServlet{
+
+	  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			  throws ServletException, IOException {
+		  
+		  
+		  
+	  }
+}
